6084064cc17fe30cee3e3f42165e948e8f266904
Mahmoudramadan21 <mahmoud.fci25@gmail.com>
2025-06-11 09:30:00 +0300

    Update Features and Company components

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import "./Company.css"
+/**
+ * Company component renders a list of company-related navigation links.
+ * @returns {JSX.Element} The Company navigation list.
+ */
+import React from "react";
+import styles from "./Company.module.css";
 
 function Company() {
-    return (
-        <ul className="inner-links company">
-            <li><a href="#">History</a></li>
-            <li><a href="#">Our Team</a></li>
-            <li><a href="#">Blog</a></li>
-        </ul>
-    )
+  return (
+    <ul className={styles["company__list"]} aria-label="Company menu">
+      <li className={styles["company__item"]}>
+        <a href="#" className={styles["company__link"]}>
+          History
+        </a>
+      </li>
+      <li className={styles["company__item"]}>
+        <a href="#" className={styles["company__link"]}>
+          Our Team
+        </a>
+      </li>
+      <li className={styles["company__item"]}>
+        <a href="#" className={styles["company__link"]}>
+          Blog
+        </a>
+      </li>
+    </ul>
+  );
 }
 
-export default Company;
+export default React.memo(Company);
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,39 +1,79 @@
-import React from 'react'
-import "./Features.css"
-import toDoList from "../../Images/icon-todo.svg"
-import calender from "../../Images/icon-calendar.svg"
-import reminders from "../../Images/icon-reminders.svg"
-import planning from "../../Images/icon-planning.svg"
+/**
+ * Features component renders a list of feature links with icons.
+ * @returns {JSX.Element} The Features navigation list.
+ */
+import React from "react";
+import styles from "./Features.module.css";
+import toDoList from "../../Images/icon-todo.svg";
+import calendar from "../../Images/icon-calendar.svg";
+import reminders from "../../Images/icon-reminders.svg";
+import planning from "../../Images/icon-planning.svg";
 
 function Features() {
-    return (
-        <ul className="inner-links features">
-            <li>
-                <div className="icon">
-                    <img src={toDoList} alt="ToDo List" />
-                </div>
-                <a href="#">ToDo List</a>
-            </li>
-            <li>
-                <div className="icon">
-                    <img src={calender} alt="Calender" />
-                </div>
-                <a href="#">calender</a>
-            </li>
-            <li>
-                <div className="icon">
-                    <img src={reminders} alt="reminders" />
-                </div>
-                <a href="#">reminders</a>
-            </li>
-            <li>
-                <div className="icon">
-                    <img src={planning} alt="planning" />
-                </div>
-                <a href="#">planning</a>
-            </li>
-        </ul>
-    )
+  return (
+    <ul className={styles["features__list"]} aria-label="Features menu">
+      <li className={styles["features__item"]}>
+        <div
+          className={`${styles["features__icon"]} ${styles["features__icon--todo"]}`}
+        >
+          <img
+            src={toDoList}
+            alt="ToDo List icon"
+            className={styles["features__icon-img"]}
+            loading="lazy"
+          />
+        </div>
+        <a href="#" className={styles["features__link"]}>
+          ToDo List
+        </a>
+      </li>
+      <li className={styles["features__item"]}>
+        <div
+          className={`${styles["features__icon"]} ${styles["features__icon--calendar"]}`}
+        >
+          <img
+            src={calendar}
+            alt="Calendar icon"
+            className={styles["features__icon-img"]}
+            loading="lazy"
+          />
+        </div>
+        <a href="#" className={styles["features__link"]}>
+          Calendar
+        </a>
+      </li>
+      <li className={styles["features__item"]}>
+        <div
+          className={`${styles["features__icon"]} ${styles["features__icon--reminders"]}`}
+        >
+          <img
+            src={reminders}
+            alt="Reminders icon"
+            className={styles["features__icon-img"]}
+            loading="lazy"
+          />
+        </div>
+        <a href="#" className={styles["features__link"]}>
+          Reminders
+        </a>
+      </li>
+      <li className={styles["features__item"]}>
+        <div
+          className={`${styles["features__icon"]} ${styles["features__icon--planning"]}`}
+        >
+          <img
+            src={planning}
+            alt="Planning icon"
+            className={styles["features__icon-img"]}
+            loading="lazy"
+          />
+        </div>
+        <a href="#" className={styles["features__link"]}>
+          Planning
+        </a>
+      </li>
+    </ul>
+  );
 }
 
-export default Features;
+export default React.memo(Features);
