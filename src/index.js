b377e52073dad6bb5efd3a193c258981b759c40f
Mahmoudramadan21 <mahmoud.fci25@gmail.com>
2025-06-11 12:00:00 +0300

    General improvements to App.css and index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+/**
+ * index.js - The entry point of the React application.
+ * This file initializes the ReactDOM and renders the App component into the DOM.
+ * @module index
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+/**
+ * Initializes the React application by creating a root and rendering the App component.
+ * The root is attached to the DOM element with id 'root'.
+ */
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* Render the App component with StrictMode for additional checks and warnings */}
     <App />
   </React.StrictMode>
 );
